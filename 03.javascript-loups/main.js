7f34bb8add6357bbc6bd8d72cef7a48b5a04a2e5
qasimovaleman <qasimovaleman2323@gmail.com>
2023-11-01 19:46:34 +0400

    JS loops

---
+++ 03.javascript-loups/main.js
@@ -0,0 +1,59 @@
+// 1-100 araliginda sadece cut reqemleri cap edin.
+// for (let i = 1; i < 100; i++) {
+//   if (i % 2 === 0) console.log(i);
+// }
+
+//Oz adinizi herflerine parcalayin
+// let fullName = "leman";
+// for (i = 0; i < fullName.length; i++) {
+//   console.log(fullName[i]);
+// }
+//Son reqemi 7 olan butun iki reqemli ededleri capa veren proqram tertib edin
+
+// for (let i = 17; i < 99; i++) {
+//   if (i % 10 === 7) console.log(i);
+// }
+
+//Reqemleri eyni olan butun iki reqemli ededleri capa veren proqram tertib edin.
+// for (let i = 11; i < 100; i++) {
+//   if (i % 11 === 0) console.log(i);
+// }
+//Verilmis ededin butun bolenlerini tapan proqram tertib edin.
+// let number = 20;
+// for (i = 0; i <= 20; i++) {
+//   if (number % i === 0) console.log(i);
+// }
+// find n factorial
+// let number = 7;
+// let factorial = 1;
+
+// for (let i = 1; i <= number; i++) {
+//   factorial = factorial * i;
+// }
+// console.log(factorial);
+
+//1-den 10-a qeder cut ededlerin cemini tapan proqram yazin.
+// let sum = 0;
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+//10-50 araliginda olan sadece 3-e bolunen ededlerin cemini tapin
+// let sum = 0;
+// for (let i = 11; i < 50; i++) {
+//   if (i % 3 === 0) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// 
+// HOMEWORK 1
+let number= 342
+let a = 3;
+let b = 4;
+let c = 2;
+console.log(`${a} + ${b} + ${c}= ${a+b+c}`)
