b9e586ed6f7d95bfa4b28baf331ef2fb84039776
qasimovaleman <qasimovaleman2323@gmail.com>
2023-11-02 19:41:37 +0400

    JS main

---
+++ 04.math/main.js
@@ -0,0 +1,29 @@
+//Math
+
+// floor;
+// ceil;
+// round;
+// trunc;
+// min;
+// max;
+// sign;
+// pow;
+// sqrt;
+// abs;
+// random;
+
+
+
+
+let number = 17.23;
+// console.log(Math.floor(number)); //17 
+
+// console.log(Math.ceil(number)); // 18
+
+// console.log(Math.round(number)); // 17
+
+
+// console.log(Math.random()*(max-min) +(min)); DUSTUR 
+// console.log(Math.ceil(Math.random() * 30 + 20));  20-50
+
+
