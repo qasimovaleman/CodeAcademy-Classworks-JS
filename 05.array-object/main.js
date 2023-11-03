f3571d76cbd16d8bc6bb098f1e6d41dbe4810a74
qasimovaleman <qasimovaleman2323@gmail.com>
2023-11-03 20:06:04 +0400

    JS array task

---
+++ 05.array-object/main.js
@@ -0,0 +1,28 @@
+//-----------------------TASK-1
+
+// const numbers = [12, 45, 6, 78, 34, 99, 23];
+// console.log("en kicik reqem", Math.min(...numbers));
+// console.log("en boyuk reqem",Math.max(...numbers));
+
+//------------------------TASK-2
+// let sum = 0;
+// let numbers = [12, 45, 6, 78, 34, 99, 23];
+
+// for (let i = 0; i < numbers.length; i++)
+// {
+//   sum += numbers[i];
+// }
+// console.log("sum:", sum);
+//-------------------------TASK-3
+// const elements = [1, 2, 3, 4, 5];
+// elements[1] = 3;
+// elements[2] = 2;
+// console.log(elements);
+//-------------------------TASK-4
+const numbers = [12, -5, 6, -3, 34, -1, 23];
+let array=[];
+numbers = array;
+for (i = 0; i > 0; i++) {
+  numbers = array;
+}
+console.log(numbers[i]);
