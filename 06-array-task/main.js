479ae25b40e6c31e55a470428ae5c81900d1a30e
qasimovaleman <qasimovaleman2323@gmail.com>
2023-11-04 17:44:26 +0400

    Js task

---
+++ 06-array-task/main.js
@@ -0,0 +1,23 @@
+let newArray = [];
+let ItCompanles = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
+console.log(ItCompanles.length);
+console.log(ItCompanles);
+console.log(ItCompanles[0]);
+console.log(ItCompanles[ItCompanles.length - 1]);
+
+console.log(ItCompanles[parseInt(ItCompanles.length / 2)]);
+let arr = [];
+for (let i = 0; i < ItCompanles.length; i++) {
+  arr.push(ItCompanles[i].toLocaleUpperCase());
+}
+console.log(arr);
+console.log(ItCompanles.toString());
+console.log(`${ItCompanles.toString()} Boyuk IT wirketleridir`);
