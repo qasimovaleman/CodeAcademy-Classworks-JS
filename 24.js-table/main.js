5ad9d019c46767bbd92cd1dbf7aad32f6ce8f8e4
qasimovaleman <qasimovaleman@2323gmail.com>
2023-11-30 21:55:52 +0400

    js table created

--- 24.js-table/main.js
+++ 24.js-table/main.js
@@ -1,4 +1,4 @@
-const users = [
+let users = [
   {
     id: 1,
     first_name: "Erda",
@@ -230,6 +230,475 @@ const users = [
     email: "hstancliffew@nps.gov",
     gender: "Male",
   },
+  {
+    id: 34,
+    first_name: "Zelma",
+    last_name: "McRannell",
+    email: "zmcrannellx@cnbc.com",
+    gender: "Female",
+  },
+  {
+    id: 35,
+    first_name: "Sophey",
+    last_name: "MacKee",
+    email: "smackeey@boston.com",
+    gender: "Female",
+  },
+  {
+    id: 36,
+    first_name: "Gwennie",
+    last_name: "Ascroft",
+    email: "gascroftz@ted.com",
+    gender: "Female",
+  },
+  {
+    id: 37,
+    first_name: "Tresa",
+    last_name: "Leney",
+    email: "tleney10@vinaora.com",
+    gender: "Female",
+  },
+  {
+    id: 38,
+    first_name: "Trey",
+    last_name: "Tivolier",
+    email: "ttivolier11@usda.gov",
+    gender: "Male",
+  },
+  {
+    id: 39,
+    first_name: "Keefer",
+    last_name: "Kroin",
+    email: "kkroin12@japanpost.jp",
+    gender: "Non-binary",
+  },
+  {
+    id: 40,
+    first_name: "Colman",
+    last_name: "Piel",
+    email: "cpiel13@craigslist.org",
+    gender: "Male",
+  },
+  {
+    id: 41,
+    first_name: "Curcio",
+    last_name: "Whybray",
+    email: "cwhybray14@newsvine.com",
+    gender: "Male",
+  },
+  {
+    id: 42,
+    first_name: "Murielle",
+    last_name: "Dreger",
+    email: "mdreger15@sun.com",
+    gender: "Female",
+  },
+  {
+    id: 43,
+    first_name: "Oralee",
+    last_name: "Coslitt",
+    email: "ocoslitt16@ft.com",
+    gender: "Female",
+  },
+  {
+    id: 44,
+    first_name: "Louise",
+    last_name: "Dinkin",
+    email: "ldinkin17@columbia.edu",
+    gender: "Bigender",
+  },
+  {
+    id: 45,
+    first_name: "Dacy",
+    last_name: "Tennewell",
+    email: "dtennewell18@topsy.com",
+    gender: "Female",
+  },
+  {
+    id: 46,
+    first_name: "Jamill",
+    last_name: "Bubear",
+    email: "jbubear19@drupal.org",
+    gender: "Male",
+  },
+  {
+    id: 47,
+    first_name: "Pauletta",
+    last_name: "Pydcock",
+    email: "ppydcock1a@scribd.com",
+    gender: "Female",
+  },
+  {
+    id: 48,
+    first_name: "Lalo",
+    last_name: "Ashbe",
+    email: "lashbe1b@businessinsider.com",
+    gender: "Male",
+  },
+  {
+    id: 49,
+    first_name: "Adena",
+    last_name: "Lembrick",
+    email: "alembrick1c@economist.com",
+    gender: "Female",
+  },
+  {
+    id: 50,
+    first_name: "Marline",
+    last_name: "Mityushin",
+    email: "mmityushin1d@opensource.org",
+    gender: "Female",
+  },
+  {
+    id: 51,
+    first_name: "Darius",
+    last_name: "Padbury",
+    email: "dpadbury1e@prnewswire.com",
+    gender: "Male",
+  },
+  {
+    id: 52,
+    first_name: "Mitzi",
+    last_name: "Pickavance",
+    email: "mpickavance1f@people.com.cn",
+    gender: "Female",
+  },
+  {
+    id: 53,
+    first_name: "Zilvia",
+    last_name: "Northgraves",
+    email: "znorthgraves1g@theguardian.com",
+    gender: "Female",
+  },
+  {
+    id: 54,
+    first_name: "Niels",
+    last_name: "De Gouy",
+    email: "ndegouy1h@bbc.co.uk",
+    gender: "Male",
+  },
+  {
+    id: 55,
+    first_name: "Freeman",
+    last_name: "Dyet",
+    email: "fdyet1i@php.net",
+    gender: "Male",
+  },
+  {
+    id: 56,
+    first_name: "Mickie",
+    last_name: "Bridgement",
+    email: "mbridgement1j@alexa.com",
+    gender: "Female",
+  },
+  {
+    id: 57,
+    first_name: "Ly",
+    last_name: "Orhtmann",
+    email: "lorhtmann1k@wikispaces.com",
+    gender: "Male",
+  },
+  {
+    id: 58,
+    first_name: "Ellynn",
+    last_name: "Causer",
+    email: "ecauser1l@istockphoto.com",
+    gender: "Female",
+  },
+  {
+    id: 59,
+    first_name: "Aron",
+    last_name: "Ughetti",
+    email: "aughetti1m@over-blog.com",
+    gender: "Male",
+  },
+  {
+    id: 60,
+    first_name: "Effie",
+    last_name: "Pannaman",
+    email: "epannaman1n@craigslist.org",
+    gender: "Female",
+  },
+  {
+    id: 61,
+    first_name: "Herschel",
+    last_name: "Piechnik",
+    email: "hpiechnik1o@soup.io",
+    gender: "Agender",
+  },
+  {
+    id: 62,
+    first_name: "Chris",
+    last_name: "Stoving",
+    email: "cstoving1p@patch.com",
+    gender: "Female",
+  },
+  {
+    id: 63,
+    first_name: "Marshal",
+    last_name: "Hercules",
+    email: "mhercules1q@mediafire.com",
+    gender: "Male",
+  },
+  {
+    id: 64,
+    first_name: "Trev",
+    last_name: "Sutherel",
+    email: "tsutherel1r@whitehouse.gov",
+    gender: "Male",
+  },
+  {
+    id: 65,
+    first_name: "Broderic",
+    last_name: "Auckland",
+    email: "bauckland1s@columbia.edu",
+    gender: "Male",
+  },
+  {
+    id: 66,
+    first_name: "Jean",
+    last_name: "McOwan",
+    email: "jmcowan1t@4shared.com",
+    gender: "Female",
+  },
+  {
+    id: 67,
+    first_name: "Jecho",
+    last_name: "Bowkett",
+    email: "jbowkett1u@cloudflare.com",
+    gender: "Male",
+  },
+  {
+    id: 68,
+    first_name: "Walsh",
+    last_name: "Paunton",
+    email: "wpaunton1v@yellowpages.com",
+    gender: "Male",
+  },
+  {
+    id: 69,
+    first_name: "Aimee",
+    last_name: "Lorincz",
+    email: "alorincz1w@miibeian.gov.cn",
+    gender: "Female",
+  },
+  {
+    id: 70,
+    first_name: "Angelina",
+    last_name: "Kemetz",
+    email: "akemetz1x@dot.gov",
+    gender: "Genderqueer",
+  },
+  {
+    id: 71,
+    first_name: "Sara",
+    last_name: "Crickmore",
+    email: "scrickmore1y@php.net",
+    gender: "Female",
+  },
+  {
+    id: 72,
+    first_name: "Jeremie",
+    last_name: "Liles",
+    email: "jliles1z@squarespace.com",
+    gender: "Genderfluid",
+  },
+  {
+    id: 73,
+    first_name: "Trace",
+    last_name: "Alvarado",
+    email: "talvarado20@elpais.com",
+    gender: "Male",
+  },
+  {
+    id: 74,
+    first_name: "Veradis",
+    last_name: "Wyld",
+    email: "vwyld21@bbb.org",
+    gender: "Female",
+  },
+  {
+    id: 75,
+    first_name: "Clywd",
+    last_name: "Harhoff",
+    email: "charhoff22@ifeng.com",
+    gender: "Male",
+  },
+  {
+    id: 76,
+    first_name: "Mannie",
+    last_name: "Georger",
+    email: "mgeorger23@seesaa.net",
+    gender: "Male",
+  },
+  {
+    id: 77,
+    first_name: "Wendell",
+    last_name: "Illingsworth",
+    email: "willingsworth24@webmd.com",
+    gender: "Male",
+  },
+  {
+    id: 78,
+    first_name: "Margeaux",
+    last_name: "Krug",
+    email: "mkrug25@tumblr.com",
+    gender: "Female",
+  },
+  {
+    id: 79,
+    first_name: "Brig",
+    last_name: "Abbay",
+    email: "babbay26@storify.com",
+    gender: "Male",
+  },
+  {
+    id: 80,
+    first_name: "Fancy",
+    last_name: "Golborn",
+    email: "fgolborn27@independent.co.uk",
+    gender: "Female",
+  },
+  {
+    id: 81,
+    first_name: "Nonah",
+    last_name: "Boughtflower",
+    email: "nboughtflower28@cocolog-nifty.com",
+    gender: "Female",
+  },
+  {
+    id: 82,
+    first_name: "Blinni",
+    last_name: "Denerley",
+    email: "bdenerley29@slashdot.org",
+    gender: "Female",
+  },
+  {
+    id: 83,
+    first_name: "Lovell",
+    last_name: "Pittwood",
+    email: "lpittwood2a@oaic.gov.au",
+    gender: "Male",
+  },
+  {
+    id: 84,
+    first_name: "Delly",
+    last_name: "Keson",
+    email: "dkeson2b@rediff.com",
+    gender: "Female",
+  },
+  {
+    id: 85,
+    first_name: "Brien",
+    last_name: "Visco",
+    email: "bvisco2c@pbs.org",
+    gender: "Bigender",
+  },
+  {
+    id: 86,
+    first_name: "Colleen",
+    last_name: "Radsdale",
+    email: "cradsdale2d@desdev.cn",
+    gender: "Female",
+  },
+  {
+    id: 87,
+    first_name: "Robena",
+    last_name: "Blencowe",
+    email: "rblencowe2e@java.com",
+    gender: "Female",
+  },
+  {
+    id: 88,
+    first_name: "Gabriel",
+    last_name: "Dadge",
+    email: "gdadge2f@sfgate.com",
+    gender: "Female",
+  },
+  {
+    id: 89,
+    first_name: "Huey",
+    last_name: "Madgewick",
+    email: "hmadgewick2g@craigslist.org",
+    gender: "Male",
+  },
+  {
+    id: 90,
+    first_name: "Filippo",
+    last_name: "Coldman",
+    email: "fcoldman2h@google.ca",
+    gender: "Male",
+  },
+  {
+    id: 91,
+    first_name: "Letty",
+    last_name: "Desorts",
+    email: "ldesorts2i@nydailynews.com",
+    gender: "Female",
+  },
+  {
+    id: 92,
+    first_name: "Thurston",
+    last_name: "Willimont",
+    email: "twillimont2j@moonfruit.com",
+    gender: "Male",
+  },
+  {
+    id: 93,
+    first_name: "Amity",
+    last_name: "Artiss",
+    email: "aartiss2k@phoca.cz",
+    gender: "Female",
+  },
+  {
+    id: 94,
+    first_name: "Ermentrude",
+    last_name: "Kornyakov",
+    email: "ekornyakov2l@disqus.com",
+    gender: "Female",
+  },
+  {
+    id: 95,
+    first_name: "Jenda",
+    last_name: "Beathem",
+    email: "jbeathem2m@prlog.org",
+    gender: "Female",
+  },
+  {
+    id: 96,
+    first_name: "Odell",
+    last_name: "Ivens",
+    email: "oivens2n@mtv.com",
+    gender: "Male",
+  },
+  {
+    id: 97,
+    first_name: "Janaye",
+    last_name: "Morrish",
+    email: "jmorrish2o@bbb.org",
+    gender: "Female",
+  },
+  {
+    id: 98,
+    first_name: "Kiah",
+    last_name: "Pinkney",
+    email: "kpinkney2p@oaic.gov.au",
+    gender: "Female",
+  },
+  {
+    id: 99,
+    first_name: "Calvin",
+    last_name: "Stichall",
+    email: "cstichall2q@craigslist.org",
+    gender: "Male",
+  },
+  {
+    id: 100,
+    first_name: "Freemon",
+    last_name: "Moggan",
+    email: "fmoggan2r@unesco.org",
+    gender: "Male",
+  },
 ];
 ////////////////////////////////////////////////////////////////
 let tBody = document.querySelector("tbody");
@@ -239,7 +708,7 @@ function drawTable(data) {
     let trElement = document.createElement("tr");
     trElement.innerHTML = `
         <td>${element.id}</td>
-        <td>${element.first_name} ,${element.last_name}</td>
+        <td>${element.first_name} ${element.last_name}</td>
         
         <td>${element.email}</td>
         <td>${element.gender}</td>`;
@@ -247,3 +716,74 @@ function drawTable(data) {
   });
 }
 drawTable(users);
+//
+let search = document.querySelector("#search");
+search.addEventListener("input", function (event) {
+  let filtered = users.filter((item) =>
+    (
+      item.first_name.toLocaleLowerCase() +
+      "" +
+      item.last_name.toLocaleLowerCase()
+    ).includes(event.target.value.toLocaleLowerCase())
+  );
+
+  drawTable(filtered);
+});
+//////////////////////////////////////////////////////////////////////////////////////////////
+let ascendingOrder = true; // Başlangıçta sıralama durumu: artan (ascending)
+let idHeader = document.querySelector("th:first-child");
+let idIcon = idHeader.querySelector("i");
+
+function toggleSortOrder() {
+  ascendingOrder = !ascendingOrder;
+  idIcon.classList.toggle("fa-arrow-down-long", ascendingOrder);
+  idIcon.classList.toggle("fa-arrow-up-long", !ascendingOrder);
+  idHeader.style.color = ascendingOrder ? "green" : "red";
+  idIcon.style.color = ascendingOrder ? "green" : "red";
+}
+
+idHeader.addEventListener("click", function () {
+  // Sıralama ikonu değişimi
+  toggleSortOrder();
+
+  // ID sıralaması
+  users.sort((a, b) => (ascendingOrder ? a.id - b.id : b.id - a.id));
+
+  // Tabloyu güncelle
+  drawTable(users);
+});
+/////////////////////////////////////////////////////////////////////////////////////////
+
+let nameHeader = document.querySelector("th:nth-child(2)");
+let iElem = document.createElement("i");
+nameHeader.append(iElem);
+nameHeader.addEventListener("click", function () {
+  // Sıralama ikonu değişimi
+  nameHeader.classList.toggle("ascending");
+  // Name sıralaması
+  if (nameHeader.classList.contains("ascending")) {
+    users.sort((a, b) =>
+      (a.first_name + " " + a.last_name).localeCompare(
+        b.first_name + " " + b.last_name
+      )
+    );
+  } else {
+    users.sort((a, b) =>
+      (b.first_name + " " + b.last_name).localeCompare(
+        a.first_name + " " + a.last_name
+      )
+    );
+  }
+  ///////////////////////////////////
+
+  function toggleOrder() {
+    ascendingOrder = !ascendingOrder;
+    iElem.classList.toggle("fa-arrow-down-long", ascendingOrder);
+    iElem.classList.toggle("fa-arrow-up-long", !ascendingOrder);
+    nameHeader.style.color = ascendingOrder ? "green" : "red";
+    nameHeader.style.color = ascendingOrder ? "green" : "red";
+  }
+  toggleOrder();
+  // Tabloyu güncelle
+  drawTable(users);
+});
